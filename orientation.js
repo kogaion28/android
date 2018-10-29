8c1d72cfe122e66b45777595791fc8e2ae272a57
Bogdan Jude <43954492+kogaion28@users.noreply.github.com>
2018-10-29 16:01:46 +0200

    Add files via upload

--- orientation.js
+++ orientation.js
@@ -1,5 +1,5 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.2";
-window.addEventListener("deviceorientation",on_device_orientarion)
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.3";
+window.addEventListener("deviceorientation",on_device_orientarion);
 
 function on_device_orientarion(e)
 
@@ -8,3 +8,10 @@ function on_device_orientarion(e)
 	document.getElementById("id_beta").innerHTML = e.beta;
 	document.getElementById("id_gamma") . innerHTML = e.gamma;
 }
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+		context.clearReact(0 , 0 canvas.width ,canvas.height )
+		R = 10 ;
+	context.beginPath();
+	context.arc(canvas.width / 2 + e.gamma /90 * (canvas. width) / 2-R) ,canvas.height / 2 + e.beta / 90 * (canvas.height) , R ,0 ,2* Math .PI )
+	context.stroke();
